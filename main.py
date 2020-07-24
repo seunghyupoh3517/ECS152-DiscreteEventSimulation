ed595f8a3e1eb376895a79e88c356977f3232ede
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2020-07-24 16:02:31 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,105 +3,206 @@
 import queue
 import random
 import math
-import Packet
-import Buffer
-import Server
+from Packet import Packet
 import GlobalEventList
+import matplotlib.pyplot as plt
 import Event
 
-# time variable contained
-class Packet:
-    def __init__(self, time): #current time
-        self.time = time
-    def getTime(self):
-        return self.time
-
-# Double Linked List sorted in increasing order
-# https://www.geeksforgeeks.org/create-doubly-linked-list-using-double-pointer-inserting-nodes-list-remains-ascending-order/
-# sorted insert in doubly linked list with head/tail pointer
-class Global_Event_List:
-    #head = None
-    #tail = None
-    def __init__(self, time, head, tail ):
-        self.time = Event.time
-        self.head = head
-        self.tail = tail
-
-    def insert(self, time, bool):
-        event1  = Event(time, None, None, bool)
-        # No event yet
-        if self.head is None:
-            self.head = self.tail = event1
-        # the last event
-        else:
-            event1 = self.tail
-            # goes to the end of the GEL
-
-    def remove(self):
-        current_event = self.head
-        if current_event.next is None:
-            self.head = current_event
-            # no event left in GEL
-        else:
-            self.head = current_event.next
-            self.head.prev = None
-        return current_event
-
-# Double Linked List -> consider as node to be used in Global Event List
-class Event:
-    def __init__(self, time, next, prev, bool):
-        self.time = time
-        #event time != packet.time
-        #arrival/departure time changes in GEL
-        self.next = next
-        self.prev = prev
-        self.arrival = bool
-        #True = arrival,  False = departure
-        #self.departure = departure
-    def getEventTime(self):
-        return self.time
-    def getArrival(self):
-        return self.arrival
-
-# Queue = Buffer
-# insert at the end, remove the first packet
-#class FIFO:
-    #def __init__(self,size):
-    #    self.size = size
-    #def insert(self):
-
-    #def remove(self):
-
-    #def drop(self):
-
-    #https: // www.geeksforgeeks.org / queue - in -python /
-# Can it be running from the main(), insert = enqueue, remove = dequeue,
-# drop = if Current Buffer capacity + incoming packet size > MAXBUFFER size - Drop++, else continue
-
-# Link Processor
-# 1)idle 2)busy
-class Server:
-    def  __init(self, data):
-    # What else should be contained  in the server?
-    # any data needs to be recorded or should we just check whether it's transmitting the data or not at the moment
-    # What else should be computed here?
-
-# inter-arrival time: use lamda for rate
+# inter-arrival time: use lambda for rate
 # transmission time: use mu for rate
 def negative_exponentially_distributed_time(rate):
     # u = drand48()
     u = random.random()
     return ((-1 / rate) * math.log(1 - u))
 
-# Initializing
-def main():
-# 1. Assume mu = 1 packet/second, lamda = 0.1, 0.2, 0.4, 0.5, 0.6, 0.80, 0.90 packets/second when MAXBUFFER = inf
-
-# 2. Mathematically compute the mean queue lengths, server utilization - compare with 1.
-
-# 3. Assume mu = 1 packet/second, lamda = 0.2, 0.4, 0.5, 0.6, 0.8, 0.9 packets/second when MAXBUFFER = 1,20, 30
-
-    que = queue.Queue() # MAXBUFFER size
 
 
-    #for i in range(0, 100000):
+
+def experiment_tester(arrival_rate_list, buffer_size):
+    mean_queue_length_list = []
+    utilization_list = []
+    drop_list = []
+
+    for arrival_rate in arrival_rate_list:
+        # arrival_rate = arrival_rate_list[6]
+        MAXBUFFER = buffer_size
+        # Initializing
+        # number of packets in the queue, by default 0
+        # current time, by default 0
+        # set service rate / arrival rate of the packets
+        # Create the first arrival event and then insert it into the GEL
+        # The event time of the first arrival event is obtained by adding inter-arrival to the current time
+        length = 0
+        time = 0
+        buffer = queue.Queue(MAXBUFFER)
+        GEL = GlobalEventList.Global_Event_List()
+
+        # insert first arrival packet into the GEL
+        arr_time = negative_exponentially_distributed_time(arrival_rate)
+        pak = Packet(negative_exponentially_distributed_time(service_rate),0)
+        GEL.insert(time + arr_time, True, pak)
+
+
+        # Statistics
+        # Utilization: what fraction of time server busy -> keep running count of the times server is busy
+        # time for server busy / total time
+        # Mean queue length: sum of the area under the curve / total time
+        # Number of packets  dropped: keep running  count of packets dropped, when it arrives at the buffer
+        pak_num = 1
+        server_busy_time = 0    # server_busy_time / time
+        prev_time = 0
+        # length_list = []
+        # time_list = []
+        # server_start = -1       # time - server_start  = server operating time
+        #                         # check when it changes
+        area = 0                # area += packet length * time
+        curr_length = 0         # curr_length = length
+        prev_length = 0         # packet length in every step -> prev_length = curr_length
+        dropped_packet = 0
+
+        # Simulation starts
+        for i in range(0, 100000):
+            # Set current time to be event time
+            # print(i)
+            current_event = GEL.remove()
+            time = current_event.time
+            width = time - prev_time                 # time interval between two events happen
+            # print(i, time, current_event.type, current_event.packet.num )
+            prev_time = time
+
+            # Arrival event process
+            if current_event.type:
+                # schedule the next arrival event and insert into GEL
+                next_packet = Packet(negative_exponentially_distributed_time(service_rate),pak_num)
+                pak_num +=1
+                GEL.insert(time + negative_exponentially_distributed_time(arrival_rate), True, next_packet)
+                # print(negative_exponentially_distributed_time(arrival_rate))
+
+                # Process Arrival Event -> deal with packet / buffer (queue) / GE
+                if length == 0:                       # server is free, transmit immediately -> create departure event
+                    GEL.insert(time + current_event.packet.service_time, False, current_event.packet)
+                    length += 1
+
+                elif length > 0:
+                    if MAXBUFFER > length - 1:        # server is busy, add packet to the queue
+                        buffer.put(current_event.packet)
+                        length += 1
+                    else:                             # exceed buffer size, drop packet
+                        dropped_packet += 1
+
+
+            # Departure event process
+            else:
+                # Process Departure Event -> deal with server / GEL
+                # set current time = event time
+                # update statistics which maintain the mean queue-length and server busy time
+                # packet departure -> decrement the length
+
+                server_busy_time += current_event.getServiceTime()     # add the service time which is the busy time of the server when process this departure packet
+                length -= 1
+
+                # if queue is empty, length ==0, do nothing
+                # if queue is not empty, length >  0
+                # 1. dequeue the first packet from the buffer
+                # 2. create new departure event  for a time, currenttime +  transmissiontime
+                # 3. insert the event in the GEL
+
+                if length == 0:             # queue is empty
+                    pass
+                    # do nothing except when the server started
+                    # if server_start != -1:
+                    #     server_busy_time += time - server_start
+                    #     server_start = -1 # reset
+
+                elif length > 0:            # queue is not empty
+                    departure_packet = buffer.get()       # remove and return an item - remove it from queue
+                    GEL.insert(time + departure_packet.service_time, False, departure_packet)
+
+            # compute statistics data
+            curr_length = length
+            area += prev_length * width
+            prev_length = curr_length
+
+
+        # Statistics
+        print("COLLECTING STATISTICS!")
+        print("arrival rate = ", arrival_rate,",  service rate = ", service_rate, ",  Buffer Size = ", MAXBUFFER)
+        print("Utilization: ", server_busy_time / time)
+        print("Mean queue length: ", area / time)
+        print("Numbers of packets dropped: ", dropped_packet)
+        print("-------------------------------------------\n")
+
+        utilization_list.append(server_busy_time / time)
+        mean_queue_length_list.append(area / time)
+        drop_list.append(dropped_packet)
+
+    experiment_results = [utilization_list, mean_queue_length_list, drop_list]
+    return experiment_results
+
+
+
+
+# main tester
+
+print("Start")
+# Experiment set loading
+arrival_rate_list = [0.1, 0.2, 0.4, 0.5, 0.6, 0.80, 0.9]
+MAXBUFFER_list = [1, 20, 30]
+service_rate = 1
+
+
+# plot 1 & 2 mean_queue_length & utilization: 7 arrival rates with infinity buffer size
+result = experiment_tester(arrival_rate_list, 9999999)
+utilization_result = result[0]
+mean_queue_result = result[1]
+
+
+# plot 3, drop packets number: 6 arrival rates with buffer size = 1
+result3 = experiment_tester(arrival_rate_list[1:6], MAXBUFFER_list[0])[2]
+
+# plot 4, drop packets number: 6 arrival rates with buffer size = 20
+result4 = experiment_tester(arrival_rate_list[1:6], MAXBUFFER_list[1])[2]
+
+# plot 5, drop packets number: 6 arrival rates with buffer size = 30
+result5 = experiment_tester(arrival_rate_list[1:6], MAXBUFFER_list[2])[2]
+
+
+# plot the statistics simulation
+fig1  = plt.figure()
+plt.plot(arrival_rate_list, mean_queue_result, marker='o')
+fig1.suptitle('Arrival Rate V.S. Mean Queue Length', fontsize=20)
+plt.xlabel('Arrival Rate', fontsize=18)
+plt.ylabel('Mean Queue Length', fontsize=16)
+fig1.savefig('mean_queue_length_plot.jpg')
+
+fig2 = plt.figure()
+plt.plot(arrival_rate_list, utilization_result, marker='o')
+fig2.suptitle('Arrival Rate V.S. Utilization', fontsize=20)
+plt.xlabel('Arrival Rate', fontsize=18)
+plt.ylabel('Utilization', fontsize=16)
+fig2.savefig('Utilization_plot.jpg')
+
+fig3 = plt.figure()
+plt.plot(arrival_rate_list[1:6], result3, marker='o')
+fig3.suptitle('Arrival Rate V.S. Drop Packets (Buffer = 1)', fontsize=20)
+plt.xlabel('Arrival Rate', fontsize=18)
+plt.ylabel('Number of dropped packets', fontsize=16)
+fig3.savefig('drop_1.jpg')
+
+
+fig4 = plt.figure()
+plt.plot(arrival_rate_list[1:6], result4, marker='o')
+fig4.suptitle('Arrival Rate V.S. Drop Packets (Buffer = 20)', fontsize=20)
+plt.xlabel('Arrival Rate', fontsize=18)
+plt.ylabel('Number of dropped packets', fontsize=16)
+fig4.savefig('drop_20.jpg')
+
+
+fig5 = plt.figure()
+plt.plot(arrival_rate_list[1:6], result5, marker='o')
+fig5.suptitle('Arrival Rate V.S. Drop Packets (Buffer = 30)', fontsize=20)
+plt.xlabel('Arrival Rate', fontsize=18)
+plt.ylabel('Number of dropped packets', fontsize=16)
+fig5.savefig('drop_30.jpg')
+
+plt.show()
